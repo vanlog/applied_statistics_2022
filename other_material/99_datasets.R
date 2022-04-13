069cc11287ae9931ef4f59fa3b85b01757b6d4e2
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2022-04-14 00:21:03 +0200

    add session 3 materials

---
+++ other_material/99_datasets.R
@@ -0,0 +1,14 @@
+# data sets ----------------------------------------------------------------
+
+library(dslabs)
+
+murders
+ChickWeight
+BOD
+BJsales
+EuStockMarkets
+DNase
+Formaldehyde
+Orange
+UCBAdmissions
+na_example
