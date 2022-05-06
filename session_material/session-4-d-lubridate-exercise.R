8d2a910f2eead1c5a76050d5ff69cc434261f742
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2022-05-06 13:57:39 +0200

    exercise solved

--- session_material/session-4-d-lubridate-exercise.R
+++ session_material/session-4-d-lubridate-exercise.R
@@ -15,32 +15,28 @@ arr_delay_ts
 
 # Create a single plot with the timelines (one for each carrier) of the total delay per month.
 
-
-ymd("2020-01,01")
-make_date(2020,01,01)
-
-
 df <- arr_delay_ts %>% 
   # filter(carrier == "AA") %>% 
   mutate(
-    date_month = ceiling_date(date, "month") - days(1)
+    month_date = ceiling_date(date, unit = "month") - days(1)
   ) %>% 
-  group_by(carrier, date_month) %>% 
+  group_by(carrier, month_date) %>% 
   summarise(
     arr_delay = sum(arr_delay, na.rm = TRUE)
-  )
-df
+  ) 
 
-df <- arr_delay_ts %>% 
-  group_by(carrier) %>% 
-  summarise(
-    arr_delay = median(arr_delay, na.rm = TRUE)
-  )
 
-df %>% 
-  ggplot() +
-  aes(x = carrier, y = arr_delay) +
-  geom_bar(stat = "identity")
+ggplot(df) +
+  aes(x = month_date, y = arr_delay, color = carrier) +
+  geom_line()
+
+
+
+
+
+
+
+
 
 
 
